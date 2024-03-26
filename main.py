8485229ea7a7533ea8ebe18f6d63de3d25830167
Stefan Matting <stefan@>
2024-03-27 00:01:45 +0100

    wxpython example

--- main.py
+++ main.py
@@ -317,3 +317,30 @@ class MouseController:
 
 if __name__ == '__main__':
     main()
+
+# import wx
+#
+#
+# class MainWindow(wx.Frame):
+#     def __init__(self, parent, title):
+#         wx.Frame.__init__(self, parent, title=title, size=(200, -1))
+#
+#         self.button = wx.Button(self, label="My simple app.")
+#         self.Bind(
+#             wx.EVT_BUTTON, self.handle_button_click, self.button
+#         )
+#
+#         self.sizer = wx.BoxSizer(wx.VERTICAL)
+#         self.sizer.Add(self.button)
+#
+#         self.SetSizer(self.sizer)
+#         self.SetAutoLayout(True)
+#         self.Show()
+#
+#     def handle_button_click(self, event):
+#         self.Close()
+#
+#
+# app = wx.App(False)
+# w = MainWindow(None, "Hello World")
+# app.MainLoop()
