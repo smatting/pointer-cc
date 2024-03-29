8f2299cccda766ea39b9714d473283c28eeed3ba
Stefan Matting <stefan@>
2024-03-29 22:04:25 +0100

    fix

--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@
 
 rm -f dist/pointer-cc.app.zip
 zip -r dist/pointer-cc.app.zip dist/pointer-cc.app
-scp dist/pointer-cc.app.zip "sefan:pointer-cc.app.$(date +%Y%m%d%H%M%S).zip"
+scp dist/pointer-cc.app.zip "stefan:pointer-cc-app.$(date +%Y%m%d%H%M%S).zip"
