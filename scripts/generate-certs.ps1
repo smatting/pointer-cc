3cb53e007a02dd801985ce799e958b11f08fe983
Stefan Matting <stefan.matting@gmail.com>
2024-04-16 21:13:59 +0200

    rework generate-certs.ps1

--- scripts/generate-certs.ps1
+++ scripts/generate-certs.ps1
@@ -1,18 +1,10 @@
-#!/usr/bin/env bash
-
-
 # https://stackoverflow.com/questions/84847/how-do-i-create-a-self-signed-certificate-for-code-signing-on-windows
 
 makecert -r -pe -n "CN=Stefan Matting CA" -ss CA -sr CurrentUser -a sha256 -cy authority -sky signature -sv StefanMattingCA.pvk StefanMattingCA.cer
 
-makecert -pe -n "CN=Stefan Matting SPC" -a sha256 -cy end \
-         -sky signature \
-         -ic StefanMattingCA.cer -iv StefanMattingCA.pvk \
+makecert -pe -n "CN=Stefan Matting SPC" -a sha256 -cy end `
+         -sky signature `
+         -ic StefanMattingCA.cer -iv StefanMattingCA.pvk `
          -sv StefanMattingSPC.pvk StefanMattingSPC.cer
 
 pvk2pfx -pvk StefanMattingSPC.pvk -spc StefanMattingSPC.cer -pfx StefanMattingSPC.pfx
-# then double-click to install to "Personal" (MY)
-
-certutil -user -addstore Root ./StefanMattingCA.cer
-certutil -user -addstore My StefanMattingSPC.cer
-
