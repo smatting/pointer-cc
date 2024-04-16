25eaacad6e6a0e2771de4e96a936f1ddb83178ee
Stefan Matting <stefan.matting@gmail.com>
2024-04-16 21:50:53 +0200

    unwhoops

--- scripts/package-win.ps1
+++ scripts/package-win.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
-# ISCC .\setup.iss
+ISCC .\setup.iss
 $items = Get-ChildItem .\Output\
 $firstItem = $items[0]
 signtool sign /fd sha256  /v /n "Stefan Matting SPC" /s My /t http://timestamp.digicert.com  Output\$firstItem
