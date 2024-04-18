3cc86f39ec95e67ec635730448890e92268141d3
Stefan Matting <stefan.matting@gmail.com>
2024-04-18 12:48:09 +0200

    fix 2

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,7 +6,7 @@ if (Test-Path -Path dist -PathType Container) {
   Remove-Item -Force -Recurse dist
 }
 
-pyinstaller --name pointer-cc 
+pyinstaller --name pointer-cc `
   --icon ./resources/icons.ico `
   --add-data "./pointercc/resources/logo-small.png:./pointercc/resources/" `
   --windowed main.py
